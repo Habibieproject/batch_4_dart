5986fde3d3a6969cb59e6df1220320d7c16cce2a
HabibieProject <projecthabibie@gmail.com>
2025-10-08 09:02:53 +0700

    dart loop

---
+++ loop/for_each.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> nama = ["Habibie", "Syaugie", "Aqsal"];
+  nama.forEach((element) {
+    print("Halo $element");
+  });
+}
---
+++ loop/for_in.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> nama = ["Habibie", "Syaugie", "Aqsal"];
+  for (var element in nama) {
+    print("Halo $element");
+  }
+}
